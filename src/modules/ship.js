6c2d1e1f2d05801e36b93db67e1212eb8bb22d56
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-02-26 14:06:45 +0000

    Implement ship class

---
+++ src/modules/ship.js
@@ -0,0 +1,17 @@
+class Ship {
+  constructor(name, length) {
+    this.name = name;
+    this.length = length;
+    this.hitCount = 0;
+  }
+
+  hit() {
+    this.hitCount += 1;
+  }
+
+  isSunk() {
+    return this.hitCount >= this.length;
+  }
+}
+
+module.exports = Ship;
