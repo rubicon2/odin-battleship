cee966eedad909bb9f4885efdd3982aea0b4d672
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-02-26 12:59:28 +0000

    Create ship tests

---
+++ src/test/ship.test.js
@@ -0,0 +1,26 @@
+const Ship = require('../modules/ship');
+
+describe('ship', () => {
+  test('ship length', () => {
+    const ship = new Ship('Name', 3);
+    expect(ship.length).toBe(3);
+  });
+
+  test('times hit', () => {
+    const ship = new Ship('Name', 3);
+    expect(ship.hitCount).toBe(0);
+  });
+
+  test('hit method', () => {
+    const ship = new Ship('Name', 3);
+    ship.hit();
+    ship.hit();
+    expect(ship.hitCount).toBe(2);
+  });
+
+  test('is sunk method', () => {
+    const ship = new Ship('Name', 1);
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
+});
